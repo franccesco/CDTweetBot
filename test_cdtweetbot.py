b12db83861c68af49a182689e9fd427a95925bc6
Franccesco Orozco <francc3sco@gmail.com>
2018-02-27 14:50:51 -0600

    add test for get_links

--- test_cdtweetbot.py
+++ test_cdtweetbot.py
@@ -1,8 +1,17 @@
 import unittest
+from cdtweetbot import get_links
 
 
 class TestCDbot(unittest.TestCase):
     """Class that tests cdtweetbot.py"""
 
-    def test_get_posts():
-        pass
+    def test_get_posts(self):
+        """Test if links are ordered correctly."""
+        links = get_links()
+        links_amount = len(links)
+        links_type = type(links)
+        self.assertTrue(links_type == dict)
+        self.assertGreater(links_amount, 1)
+
+
+unittest.main()
